215d8c00328707f64590ab02fe20547a133786ce
Shawn Miller <shawn@mediocre.com>
2013-11-15 10:18:31 -0600

    Removed unused hashtag code

--- test/test.js
+++ test/test.js
@@ -18,23 +18,6 @@ describe('newlines', function() {
     });
 });
 
-/*describe('hashtags', function() {
-    it(' #tag', function() {
-        var text = mehdown.parse(' #tag');
-        assert.equal(text, ' <a href="https://mediocre.com/tags/tag">#tag</a>');
-    });
-
-    it('abc #tag 123', function() {
-        var text = mehdown.parse('abc #tag 123');
-        assert.equal(text, 'abc <a href="https://mediocre.com/tags/tag">#tag</a> 123');
-    });
-
-    it('abc #tag-1 not-a-tag#not-a-tag #tag-2 123', function() {
-        var text = mehdown.parse('abc #tag-1 not-a-tag#not-a-tag #tag-2 123');
-        assert.equal(text, 'abc <a href="https://mediocre.com/tags/tag-1">#tag-1</a> not-a-tag#not-a-tag <a href="https://mediocre.com/tags/tag-2">#tag-2</a> 123');
-    });
-});*/
-
 describe('usernames', function() {
     it('@username', function() {
         var text = mehdown.parse('@username');
