defcc670be244b75e036e8994e7c3956f0e52c81
katy lavallee <katylava@gmail.com>
2014-03-04 16:27:44 -0600

    Change all instances of localhost to 127.0.0.1

--- test/test.js
+++ test/test.js
@@ -39,9 +39,9 @@ describe('anchors', function() {
         assert.equal(text, '<p><a href="/path">path</a></p>');
     });
 
-    it('localhost href', function() {
-        var text = mehdown.parse('<p><a href="http://localhost:8000/path">path</a></p>');
-        assert.equal(text, '<p><a href="http://localhost:8000/path">path</a></p>');
+    it('127.0.0.1 href', function() {
+        var text = mehdown.parse('<p><a href="http://127.0.0.1:8000/path">path</a></p>');
+        assert.equal(text, '<p><a href="http://127.0.0.1:8000/path">path</a></p>');
     });
 
     it('mediocre href', function() {
@@ -60,17 +60,17 @@ describe('strikethrough', function() {
 describe('usernames', function() {
     it('@username', function() {
         var text = mehdown.parse('@username');
-        assert.equal(text, '<a href="http://localhost:8000/@username">@username</a>');
+        assert.equal(text, '<a href="http://127.0.0.1:8000/@username">@username</a>');
     });
 
     it('abc @username 123', function() {
         var text = mehdown.parse('abc @username 123');
-        assert.equal(text, 'abc <a href="http://localhost:8000/@username">@username</a> 123');
+        assert.equal(text, 'abc <a href="http://127.0.0.1:8000/@username">@username</a> 123');
     });
 
     it('abc @username1 notausername@notausername @username2 123', function() {
         var text = mehdown.parse('abc @username1 notausername@notausername @username2 123');
-        assert.equal(text, 'abc <a href="http://localhost:8000/@username1">@username1</a> notausername@notausername <a href="http://localhost:8000/@username2">@username2</a> 123');
+        assert.equal(text, 'abc <a href="http://127.0.0.1:8000/@username1">@username1</a> notausername@notausername <a href="http://localhost:8000/@username2">@username2</a> 123');
     });
 });
 
