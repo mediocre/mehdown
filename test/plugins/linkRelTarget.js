460925693f0c25482801c3986ac0a3fce3b13bfd
Shawn Miller <shawn@mediocre.com>
2016-05-18 18:34:47 -0500

    more linkRelTarget tests

--- test/plugins/linkRelTarget.js
+++ test/plugins/linkRelTarget.js
@@ -2,33 +2,46 @@ const assert = require('assert');
 
 const mehdown = require('../../lib');
 
-describe('anchors', function() {
-    it('rel attributes', function(done) {
-        mehdown.render('[Google](http://www.google.com)', { baseUrl: 'https://mediocre.com' }, function(err, html) {
-            assert.equal(html, '<p><a href="http://www.google.com" rel="nofollow" target="_blank">Google</a></p>');
+describe('linksRelTarget', function() {
+    it('should not set rel attribute or target attribute if there is no baseUrl specified', function(done) {
+        mehdown.render('[Google](https://www.google.com)', function(err, html) {
+            assert.equal(html, '<p><a href="https://www.google.com">Google</a></p>');
             done();
         });
     });
 
-    it.skip('target attributes', function() {
-        var text = mehdown.parse('<p><a target="abc" href="http://www.google.com">Google</a></p>');
-        assert.equal(text, '<p><a rel="nofollow" target="_blank" href="http://www.google.com">Google</a></p>');
+    it('should set rel attribute to "nofollow" and target attribute to "_blank" for a URL that is not at the same domain as the base URL', function(done) {
+        mehdown.render('[Google](https://www.google.com)', { baseUrl: 'https://mediocre.com' }, function(err, html) {
+            assert.equal(html, '<p><a href="https://www.google.com" rel="nofollow" target="_blank">Google</a></p>');
+            done();
+        });
     });
 
-    it.skip('local href', function() {
-        var text = mehdown.parse('<p><a href="/path">path</a></p>');
-        assert.equal(text, '<p><a href="/path">path</a></p>');
+    it('should set rel attribute to "nofollow" and target attribute to "_blank" for a scheme relative URL that is not at the same domain as the base URL', function(done) {
+        mehdown.render('[Google](//www.google.com)', { baseUrl: 'https://mediocre.com' }, function(err, html) {
+            assert.equal(html, '<p><a href="//www.google.com" rel="nofollow" target="_blank">Google</a></p>');
+            done();
+        });
     });
 
-    it.skip('127.0.0.1 href', function() {
-        mehdown.baseUrl = 'http://127.0.0.1:8000';
-        var text = mehdown.parse('<p><a href="http://127.0.0.1:8000/path">path</a></p>');
-        assert.equal(text, '<p><a href="http://127.0.0.1:8000/path">path</a></p>');
-        mehdown.baseUrl = defaultBaseUrl;
+    it('should not set rel attribute or target attribute for a local URL', function(done) {
+        mehdown.render('[path](/path)', { baseUrl: 'https://mediocre.com' }, function(err, html) {
+            assert.equal(html, '<p><a href="/path">path</a></p>');
+            done();
+        });
     });
 
-    it.skip('mediocre href', function() {
-        var text = mehdown.parse('<p><a href="https://mediocre.com/path">path</a></p>');
-        assert.equal(text, '<p><a href="https://mediocre.com/path">path</a></p>');
+    it('should not set rel attribute to "nofollow" and target attribute to "_blank" for a baseUrl with an IP address and port', function(done) {
+        mehdown.render('[path](http://127.0.0.1:8000/path)', { baseUrl: 'http://127.0.0.1:8000'}, function(err, html) {
+            assert.equal(html, '<p><a href="http://127.0.0.1:8000/path">path</a></p>');
+            done();
+        });
+    });
+
+    it('should not set rel attribute to "nofollow" and target attribute to "_blank" for a URL that is at the same domain as the base URL', function(done) {
+        mehdown.render('[path](https://mediocre.com/path)', { baseUrl: 'https://mediocre.com' }, function(err, html) {
+            assert.equal(html, '<p><a href="https://mediocre.com/path">path</a></p>');
+            done();
+        });
     });
 });
