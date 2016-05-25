e5630e5e1c9e6c70267ca36560588f05a6057c58
katy lavallee <katy@mediocre.com>
2016-05-25 15:41:59 -0500

    Separate mediocre sites into different plugins

---
+++ test/plugins/drone-horse.js
@@ -0,0 +1,61 @@
+const assert = require('assert');
+
+const mehdown = require('../../lib');
+
+describe('drone.horse', function() {
+    it('https://drone.horse/deals/a-slug', function(done) {
+        mehdown.render('https://drone.horse/deals/a-slug', function(err, html) {
+            assert.equal(html, '<p><iframe class="deals" frameborder="0" scrolling="no" src="https://drone.horse/deals/a-slug/embed"></iframe></p>');
+            done();
+        });
+    });
+
+    it('http://drone.horse/deals/a-slug', function(done) {
+        mehdown.render('http://drone.horse/deals/a-slug', function(err, html) {
+            assert.equal(html, '<p><iframe class="deals" frameborder="0" scrolling="no" src="https://drone.horse/deals/a-slug/embed"></iframe></p>');
+            done();
+        });
+    });
+
+    it('https://www.drone.horse/deals/a-slug', function(done) {
+        mehdown.render('https://www.drone.horse/deals/a-slug', function(err, html) {
+            assert.equal(html, '<p><iframe class="deals" frameborder="0" scrolling="no" src="https://drone.horse/deals/a-slug/embed"></iframe></p>');
+            done();
+        });
+    });
+
+    it('www.drone.horse/deals/a-slug', function(done) {
+        mehdown.render('www.drone.horse/deals/a-slug', function(err, html) {
+            assert.equal(html, '<p><iframe class="deals" frameborder="0" scrolling="no" src="https://drone.horse/deals/a-slug/embed"></iframe></p>');
+            done();
+        });
+    });
+
+    it('drone.horse/deals/a-slug', function(done) {
+        mehdown.render('drone.horse/deals/a-slug', function(err, html) {
+            assert.equal(html, '<p><iframe class="deals" frameborder="0" scrolling="no" src="https://drone.horse/deals/a-slug/embed"></iframe></p>');
+            done();
+        });
+    });
+
+    it('https://drone.horse/deals/a-slug/', function(done) {
+        mehdown.render('https://drone.horse/deals/a-slug/', function(err, html) {
+            assert.equal(html, '<p><iframe class="deals" frameborder="0" scrolling="no" src="https://drone.horse/deals/a-slug/embed"></iframe></p>');
+            done();
+        });
+    });
+
+    it('https://drone.horse/polls/a-slug', function(done) {
+        mehdown.render('https://drone.horse/polls/a-slug', function(err, html) {
+            assert.equal(html, '<p><iframe class="polls" frameborder="0" scrolling="no" src="https://drone.horse/polls/a-slug/embed"></iframe></p>');
+            done();
+        });
+    });
+
+    it('https://drone.horse/deals/a--slug', function(done) {
+        mehdown.render('https://drone.horse/deals/a--slug', function(err, html) {
+            assert.equal(html, '<p><iframe class="deals" frameborder="0" scrolling="no" src="https://drone.horse/deals/a--slug/embed"></iframe></p>');
+            done();
+        });
+    });
+});
--- test/plugins/mediocre-com.js
+++ test/plugins/mediocre-com.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 
 const mehdown = require('../../lib');
 
-describe('mediocre', function() {
+describe('mediocre.com', function() {
     it('https://mediocre.com/deals/a-slug', function(done) {
         mehdown.render('https://mediocre.com/deals/a-slug', function(err, html) {
             assert.equal(html, '<p><iframe class="deals" frameborder="0" scrolling="no" src="https://mediocre.com/deals/a-slug/embed"></iframe></p>');
@@ -56,28 +56,6 @@ describe('mediocre', function() {
         mehdown.render('https://mediocre.com/deals/a--slug', function(err, html) {
             assert.equal(html, '<p><iframe class="deals" frameborder="0" scrolling="no" src="https://mediocre.com/deals/a--slug/embed"></iframe></p>');
             done();
-        });
-    });
-
-    it('https://meh.com/deals/a-slug', function(done) {
-        mehdown.render('https://meh.com/deals/a-slug', function(err, html) {
-            assert.equal(html, '<p><iframe class="deals" frameborder="0" scrolling="no" src="https://meh.com/deals/a-slug/embed"></iframe></p>');
-            done();
-        });
-    });
-
-    it('https://drone.horse/deals/a-slug', function(done) {
-        mehdown.render('https://drone.horse/deals/a-slug', function(err, html) {
-            assert.equal(html, '<p><iframe class="deals" frameborder="0" scrolling="no" src="https://drone.horse/deals/a-slug/embed"></iframe></p>');
-            done();
-        });
-    });
-
-    it('https://example.com/deals/a-slug', function(done) {
-        mehdown.render('https://example.com/deals/a-slug', function(err, html) {
-            assert.equal(html, '<p><a href="https://example.com/deals/a-slug">https://example.com/deals/a-slug</a></p>');
-            done();
-
         });
     });
 });
