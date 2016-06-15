d5a4f951ec8dd86fa115f62a694ec8883d415ebc
Shawn Miller <shawn@mediocre.com>
2016-06-15 14:45:16 -0500

    Better Soundcloud support

--- test/plugins/soundcloud.js
+++ test/plugins/soundcloud.js
@@ -16,4 +16,18 @@ describe('soundcloud', function() {
             done();
         });
     });
+
+    it('soundcloud.com/shawnmichaelmiller/santa-claus-is-coming-to-town', function(done) {
+        mehdown.render('soundcloud.com/shawnmichaelmiller/santa-claus-is-coming-to-town', function(err, html) {
+            assert.equal(html, '<p><iframe class="soundcloud" frameborder="0" src="https://w.soundcloud.com/player/?visual=true&url=https%3A%2F%2Fsoundcloud.com%2Fshawnmichaelmiller%2Fsanta-claus-is-coming-to-town"></iframe></p>');
+            done();
+        });
+    });
+
+    it('www.soundcloud.com/shawnmichaelmiller/santa-claus-is-coming-to-town', function(done) {
+        mehdown.render('www.soundcloud.com/shawnmichaelmiller/santa-claus-is-coming-to-town', function(err, html) {
+            assert.equal(html, '<p><iframe class="soundcloud" frameborder="0" src="https://w.soundcloud.com/player/?visual=true&url=https%3A%2F%2Fsoundcloud.com%2Fshawnmichaelmiller%2Fsanta-claus-is-coming-to-town"></iframe></p>');
+            done();
+        });
+    });
 });
