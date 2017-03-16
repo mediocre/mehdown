b7f36ccdc125024d2eb7ab132d2cd7dfc7209376
Shawn Miller <shawn@mediocre.com>
2017-03-16 13:41:36 -0500

    Twitch

--- test/plugins/twitch.js
+++ test/plugins/twitch.js
@@ -5,7 +5,7 @@ const mehdown = require('../../lib');
 describe('twitch', function() {
     it('https://www.twitch.tv/twitchplayspokemon', function(done) {
         mehdown.render('https://www.twitch.tv/twitchplayspokemon', function(err, html) {
-            assert.equal(html, '<p><iframe allowfullscreen="true" class="twitch-video" frameborder="0" src="http://player.twitch.tv/?channel=twitchplayspokemon&muted=true"></iframe><iframe class="twitch-chat" frameborder="0" src="http://www.twitch.tv/twitchplayspokemon/chat"></iframe></p>');
+            assert.equal(html, '<p><iframe allowfullscreen="true" class="twitch-video" frameborder="0" src="https://player.twitch.tv/?channel=twitchplayspokemon&muted=true"></iframe><iframe class="twitch-chat" frameborder="0" src="https://www.twitch.tv/twitchplayspokemon/chat"></iframe></p>');
             done();
         });
     });
