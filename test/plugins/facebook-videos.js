fce1ab889010b5272367434fb0c00744a806d066
Shawn Miller <shawn@mediocre.com>
2017-05-10 17:18:28 -0500

    Facebook video parameters

--- test/plugins/facebook-videos.js
+++ test/plugins/facebook-videos.js
@@ -3,9 +3,23 @@ const assert = require('assert');
 const mehdown = require('../../lib');
 
 describe('facebook videos', function() {
+    it('https://www.facebook.com/FacebookDevelopers/videos/10152454700553553', function(done) {
+        mehdown.render('https://www.facebook.com/FacebookDevelopers/videos/10152454700553553', function(err, html) {
+            assert.equal(html, '<p><iframe allowfullscreen class="facebook video" frameborder="0" src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FFacebookDevelopers%2Fvideos%2F10152454700553553"></iframe></p>');
+            done();
+        });
+    });
+
     it('https://www.facebook.com/FacebookDevelopers/videos/10152454700553553/', function(done) {
         mehdown.render('https://www.facebook.com/FacebookDevelopers/videos/10152454700553553/', function(err, html) {
-            assert.equal(html, '<p><iframe allowfullscreen class="facebook video" frameborder="0" src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FFacebookDevelopers%2Fvideos%2F10152454700553553%2F"></iframe></p>');
+            assert.equal(html, '<p><iframe allowfullscreen class="facebook video" frameborder="0" src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FFacebookDevelopers%2Fvideos%2F10152454700553553"></iframe></p>');
+            done();
+        });
+    });
+
+    it('https://www.facebook.com/FacebookDevelopers/videos/10152454700553553?autoplay=true&show-captions=true&show-text=true', function(done) {
+        mehdown.render('https://www.facebook.com/FacebookDevelopers/videos/10152454700553553?autoplay=true&show-captions=true&show-text=true', function(err, html) {
+            assert.equal(html, '<p><iframe allowfullscreen class="facebook video" frameborder="0" src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FFacebookDevelopers%2Fvideos%2F10152454700553553&autoplay=true&show-captions=true&show-text=true"></iframe></p>');
             done();
         });
     });
