e9054cc127ad5bd0d4b88c7a19664d830c267482
Shawn Miller <shawn@mediocre.com>
2017-05-31 19:35:49 -0500

    Apple Music plugin

---
+++ test/plugins/appleMusic.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+
+const mehdown = require('../../lib');
+
+describe('Apple Music', function() {
+    it('https://itunes.apple.com/us/album/the-rescues/id1233432159', function(done) {
+        mehdown.render('https://itunes.apple.com/us/album/the-rescues/id1233432159', function(err, html) {
+            assert.equal(html, '<p><iframe frameborder="0" src="//tools.applemusic.com/embed/v1/album/1233432159"></iframe></p>');
+            done();
+        });
+    });
+});
