15f3d9fac6f27db2d8e5b2b8be178c38e88f0ddf
Shawn Miller <shawn@mediocre.com>
2017-05-31 19:48:42 -0500

    add apple-music class

--- test/plugins/appleMusic.js
+++ test/plugins/appleMusic.js
@@ -5,7 +5,7 @@ const mehdown = require('../../lib');
 describe('Apple Music', function() {
     it('https://itunes.apple.com/us/album/the-rescues/id1233432159', function(done) {
         mehdown.render('https://itunes.apple.com/us/album/the-rescues/id1233432159', function(err, html) {
-            assert.equal(html, '<p><iframe frameborder="0" src="//tools.applemusic.com/embed/v1/album/1233432159"></iframe></p>');
+            assert.equal(html, '<p><iframe class="apple-music" frameborder="0" src="//tools.applemusic.com/embed/v1/album/1233432159"></iframe></p>');
             done();
         });
     });
