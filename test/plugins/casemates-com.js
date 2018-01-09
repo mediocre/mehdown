9be2c36559ea9d6a6a0533f58a9c9cbf48275f69
Shawn Miller <shawn@mediocre.com>
2018-01-09 16:01:33 -0600

    casemates.com deal embeds

--- test/plugins/casemates-com.js
+++ test/plugins/casemates-com.js
@@ -3,6 +3,13 @@ const assert = require('assert');
 const mehdown = require('../../lib');
 
 describe('casemates.com', function() {
+    it('https://casemates.com/deals/a--slug', function(done) {
+        mehdown.render('https://casemates.com/deals/a--slug', function(err, html) {
+            assert.equal(html, '<p><iframe class="deals" frameborder="0" scrolling="no" src="https://casemates.com/deals/a--slug/embed"></iframe></p>');
+            done();
+        });
+    });
+
     it('https://casemates.com/polls/a-slug', function(done) {
         mehdown.render('https://casemates.com/polls/a-slug', function(err, html) {
             assert.equal(html, '<p><iframe class="polls" frameborder="0" scrolling="no" src="https://casemates.com/polls/a-slug/embed"></iframe></p>');
