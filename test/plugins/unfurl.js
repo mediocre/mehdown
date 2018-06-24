fbe52dbf595f2f77dc849b3830ec79a400e92bba
Shawn Miller <shawn@mediocre.com>
2018-06-24 10:12:22 -0500

    wip

---
+++ test/plugins/unfurl.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+
+const mehdown = require('../../lib');
+
+describe('unfurl', function() {
+    it('https://www.npmjs.com/package/metascraper', function(done) {
+        this.timeout(10000);
+
+        mehdown.render('https://www.npmjs.com/package/metascraper', function(err, html) {
+            assert.notEqual(html, '<p><a href="https://wine.woot.com/forums/viewpost.aspx?postid=7268481&amp;pageindex=12#post7293167">https://wine.woot.com/forums/viewpost.aspx?postid=7268481&amp;pageindex=12#post7293167</a></p>');
+            done();
+        });
+    });
+});
