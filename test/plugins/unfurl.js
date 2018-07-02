a6f0a19accca309d052a4b1ec5ce7283eca2d4a7
Shawn Miller <shawn@mediocre.com>
2018-07-02 09:47:45 -0500

    more unfurl work

--- test/plugins/unfurl.js
+++ test/plugins/unfurl.js
@@ -2,11 +2,12 @@ const assert = require('assert');
 
 const mehdown = require('../../lib');
 
-describe('unfurl', function() {
+describe.only('unfurl', function() {
     it('https://www.npmjs.com/package/metascraper', function(done) {
         this.timeout(10000);
 
         mehdown.render('https://www.npmjs.com/package/metascraper', function(err, html) {
+            console.log(html);
             assert.notEqual(html, '<p><a href="https://wine.woot.com/forums/viewpost.aspx?postid=7268481&amp;pageindex=12#post7293167">https://wine.woot.com/forums/viewpost.aspx?postid=7268481&amp;pageindex=12#post7293167</a></p>');
             done();
         });
