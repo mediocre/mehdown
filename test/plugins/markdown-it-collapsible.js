c4a1d2c65576766f771004be74762b4a354b6e95
Shawn Miller <shawn@mercatalyst.com>
2025-02-13 13:08:31 -0600

    Update markdown-it-collapsible.js

--- test/plugins/markdown-it-collapsible.js
+++ test/plugins/markdown-it-collapsible.js
@@ -12,4 +12,34 @@ describe('markdown-it-collapsible', function() {
             done();
         });
     });
+
+    it('should render expanded', function(done) {
+        const markdown = '++> Click me!\nHidden text\n++>';
+
+        mehdown.render(markdown, function(err, html) {
+            assert.ifError(err);
+            assert.strictEqual(html, '<details open="">\n<summary><span class="details-marker"></span>Click me!</summary><p>Hidden text</p>\n</details>');
+            done();
+        });
+    });
+
+    it('should support nested collapsibles', function(done) {
+        const markdown = '++++ Click me!\nHidden text\n+++ Nested\nInner hidden text\n+++\n++++';
+
+        mehdown.render(markdown, function(err, html) {
+            assert.ifError(err);
+            assert.strictEqual(html, '<details>\n<summary><span class="details-marker"></span>Click me!</summary><p>Hidden text</p>\n<details>\n<summary><span class="details-marker"></span>Nested</summary><p>Inner hidden text</p>\n</details>\n</details>');
+            done();
+        });
+    });
+
+    it('should support open nested collapsibles', function(done) {
+        const markdown = '+++> Click me!\nHidden text\n+++ Nested\nInner hidden text\n+++\n+++>';
+
+        mehdown.render(markdown, function(err, html) {
+            assert.ifError(err);
+            assert.strictEqual(html, '<details open="">\n<summary><span class="details-marker"></span>Click me!</summary><p>Hidden text</p>\n<details>\n<summary><span class="details-marker"></span>Nested</summary><p>Inner hidden text</p>\n</details>\n</details>');
+            done();
+        });
+    });
 });
